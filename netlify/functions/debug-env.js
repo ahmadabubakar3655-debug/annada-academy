37219d0a956d403cad66a7c652e2abb6bbe6c59c
Yahaya Garba <annadaacademy@gmail.com>
2026-09-10 16:53:18 +0100

    Cleaned up duplicate files and fixed function locations

--- netlify/functions/debug-env.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      mongodb_uri: process.env.MONGODB_URI ? 'Set' : 'Not set',
-      jwt_secret: process.env.JWT_SECRET ? 'Set' : 'Not set'
-    })
-  };
-};
